1578cd681423c9e6bce7fcce2893d1a4cfe0ce70
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-09-29 12:02:16 -0300

    construction: login is almost done, missing token

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -15,11 +15,11 @@ async function verifyExistEmail(email: string): Promise<users | null> {
     return emailExist;
 }
 
-function passwordConfirmPasswordMatch(password: string, confirmPassword: string) { 
+function passwordConfirmPasswordMatch(password: string, confirmPassword: string): void { 
     if(password!==confirmPassword) throw { type: "Conflit", message: "The password and confirmPassord don't match"}
 }
 
-export async function signup(userData: signUp) {
+export async function signup(userData: signUp): Promise<void> {
     passwordConfirmPasswordMatch(userData.password,userData.confirmPassword);
 
     const usernameExist: users | null = await verifyExistUsername(userData.username);
@@ -29,10 +29,32 @@ export async function signup(userData: signUp) {
     if(emailExist) throw { type: "Conflit", message: "This email is registred yet"}
 
     userData = {...userData, password: crypts.encriptByBcrypt(userData.password)}
-    console.log(userData);
     const userWithoutConfirmPassword: createUser = exclude(userData,'confirmPassword');
 
     await usersRepository.createUser(userWithoutConfirmPassword);
+}
+
+function matchPassword(passEncrypt: string, password: string): void { 
+    const verify: boolean = crypts.descriptByBcrypt(passEncrypt,password);
+
+    if(!verify) throw { type: "Unauthorized", message: "User or password are wrong"}
+}
+
+export async function login(usernameOrEmail: string, password: string) { 
+    const existEmail = await verifyExistEmail(usernameOrEmail);
+    const existUsername =  await verifyExistUsername(usernameOrEmail);
+
+    if(!existEmail && !existUsername) throw { type: "Unauthorized", message: "User or password are wrong"}
+
+    if(existEmail) {
+        const passEncrypt: string  = existEmail.password;
+        matchPassword(passEncrypt,password);
+        return existEmail;
+    } else if(existUsername) {
+        const passEncrypt: string  = existUsername.password;
+        matchPassword(passEncrypt,password);
+        return existUsername;
+    }
 }
 
 function exclude<User, Key extends keyof User>(
