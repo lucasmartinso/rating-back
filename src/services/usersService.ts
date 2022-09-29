20f1c4a86d06801b6ad5303984900a8031fdcb34
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-09-29 11:13:44 -0300

    feat: create function to verify if the password and confirmPassword match

--- src/services/usersService.ts
+++ src/services/usersService.ts
@@ -1,6 +1,7 @@
 import { users } from "@prisma/client";
 import * as usersRepository from "../repositories/usersRepository"
 import { createUser, signUp } from "../types/usersType";
+import { crypts } from "../utils/cripts/crypts"
 
 async function verifyExistUsername(username: string): Promise<users | null> { 
     const usernameExist: users | null = await usersRepository.verifyExistUsername(username);
@@ -14,13 +15,20 @@ async function verifyExistEmail(email: string): Promise<users | null> {
     return emailExist;
 }
 
+function passwordConfirmPasswordMatch(password: string, confirmPassword: string) { 
+    if(password!==confirmPassword) throw { type: "Conflit", message: "The password and confirmPassord don't match"}
+}
+
 export async function signup(userData: signUp) {
+    passwordConfirmPasswordMatch(userData.password,userData.confirmPassword);
+
     const usernameExist: users | null = await verifyExistUsername(userData.username);
     if(usernameExist) throw { type: "Conflit", message: "This username already exist"}
 
     const emailExist: users | null = await verifyExistEmail(userData.email);
     if(emailExist) throw { type: "Conflit", message: "This email is registred yet"}
 
+    userData = {...userData, password: crypts.encriptByBcrypt(userData.password)}
     const userWithoutConfirmPassword: createUser = exclude(userData,'confirmPassword');
 
     await usersRepository.createUser(userWithoutConfirmPassword);
