1a7d1f2f10afbe7908a9615c2daf2300f52d4032
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-10-03 00:04:49 -0300

    fix: putting required to almost of request

--- src/schemas/ratingSchema.ts
+++ src/schemas/ratingSchema.ts
@@ -1,9 +1,9 @@
 import joi from "joi";
 
 export const ratingSchema = joi.object({
-    food: joi.number().integer().min(1).max(5),
-    environment: joi.number().integer().min(1).max(5),
-    attendance: joi.number().integer().min(1).max(5),
-    price: joi.number().integer().min(1).max(5),
+    food: joi.number().integer().min(1).max(5).required(),
+    environment: joi.number().integer().min(1).max(5).required(),
+    attendance: joi.number().integer().min(1).max(5).required(),
+    price: joi.number().integer().min(1).max(5).required(),
     comment: joi.string()
 })
