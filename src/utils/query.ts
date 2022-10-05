54a2532f8fb549394edfeb76f65dc150a03ad6c4
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-10-05 15:46:53 -0300

    fix: add to place the number of avaluations

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -1,5 +1,5 @@
 export const querie: string = 
-`SELECT fp.id, fp.name, fp.score, fp."mainPhoto", AVG(r.food) AS food, AVG(r.environment) AS environment, AVG(r.attendance) AS attendance, AVG(r.price) AS price, t.name AS typeFood, fp.verify 
+`SELECT fp.id, fp.name, fp.score, fp."mainPhoto", AVG(r.food) AS food, AVG(r.environment) AS environment, AVG(r.attendance) AS attendance, AVG(r.price) AS price, t.name AS typeFood, COUNT(r."foodPlaceId") AS "numberRatings", fp.verify 
     FROM "foodPlaces" fp
     JOIN "ratingFoodPlaces" r ON r."foodPlaceId"=fp.id
     JOIN "typeFoodPlaces" t ON t.id=fp."typeId"
