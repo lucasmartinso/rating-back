df2b88eaf0b28148d82e39d02ddfc79400054ec2
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-10-05 15:54:21 -0300

    fix: foodType repository add number of avaluations

--- src/repositories/foodTypeRepository.ts
+++ src/repositories/foodTypeRepository.ts
@@ -3,7 +3,7 @@ import { querie } from "../utils/query";
 
 export async function filterFoodType(typeId: number): Promise<any[]> {
     const { rows: typePlaces }: any = await connection.query(`
-    SELECT fp.id, fp.name, fp.score, fp."mainPhoto", AVG(r.food) AS food, AVG(r.environment) AS environment, AVG(r.attendance) AS attendance, AVG(r.price) AS price, t.name AS typeFood, fp.verify 
+    SELECT fp.id, fp.name, fp.score, fp."mainPhoto", AVG(r.food) AS food, AVG(r.environment) AS environment, AVG(r.attendance) AS attendance, AVG(r.price) AS price, t.name AS typeFood, COUNT(r."foodPlaceId") AS "numberRatings", fp.verify 
     FROM "foodPlaces" fp
     JOIN "ratingFoodPlaces" r ON r."foodPlaceId"=fp.id
     JOIN "typeFoodPlaces" t ON t.id=fp."typeId"
