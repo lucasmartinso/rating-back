a8424f48de7755227f591a0ec35f9439ac190875
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-11-01 21:01:15 -0300

    test: sign-up 409 tests is done

--- tests/integration/sign-up.test.ts
+++ tests/integration/sign-up.test.ts
@@ -3,6 +3,7 @@ import { __createUser } from "../factories/sign-up-factory";
 import serverSupertest from "../jestConfig";
 import httpStatus from 'http-status';
 import { connectPrisma, disconnectPrisma, disconnectRedis } from "../factories/scenary-factory";
+import { faker }from "@faker-js/faker";
 
 const server = serverSupertest();
 
@@ -24,20 +25,22 @@ describe('TEST POST /sign-up', () =>  {
         const errorMessage: string = 'This username already exist';
 
         await server.post('/sign-up').send(userData);
-        const { status }: { status: number, body: string } = await server.post('/sign-up').send(userData);
-        const km = await server.post('/sign-up').send(userData);
-        console.log(km.text);
+        const { status, text }: { status: number, text: string } = await server.post('/sign-up').send(userData);
     
         expect(status).toBe(httpStatus.CONFLICT);
-        expect(km.text).toBe(errorMessage);
+        expect(text).toBe(errorMessage);
     });
 
-    it('Should answer 200, if the user send the corretly schema', async() => { 
+    it('Should answer 409, if the user send the corretly schema but email already exist', async() => { 
         const userData: signUp = await __createUser();
+        const errorMessage: string = 'This email is registred yet';
 
-        const { status }: { status: number } = await server.post('/sign-up').send(userData);
+        await server.post('/sign-up').send(userData);
+        userData.username = faker.internet.email();
+        const { status, text }: { status: number, text: string } = await server.post('/sign-up').send(userData);
     
-        expect(status).toBe(httpStatus.CREATED);
+        expect(status).toBe(httpStatus.CONFLICT);
+        expect(text).toBe(errorMessage);
     });
 });
 
