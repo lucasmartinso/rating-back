11853b2d349da9fc0ae248e1ac4f3e6909279006
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-11-21 21:52:54 -0300

    feat: create a personalized message to rating schema

---
+++ tests/schemas/rating/create-schema.test.ts
@@ -0,0 +1,35 @@
+import serverSupertest from "../../jestConfig";
+import httpStatus from 'http-status';
+import { connectPrisma, disconnectPrisma, deleteAllData } from "../../factories/scenary-factory";
+import { faker } from "@faker-js/faker";
+import { __createRating } from "../../factories/rating-factory";
+import { __createToken } from "../../factories/login-factory";
+import { ratingInfo } from "../../../src/types/ratingType";
+import { __createRestaurant } from "../../factories/place-factory";
+import { placeInfo } from "../../../src/types/placesType";
+import { foodPlaces } from "@prisma/client";
+
+const server = serverSupertest();
+
+beforeEach( async() => {
+    await connectPrisma();
+    await deleteAllData();
+});
+
+describe('TEST SCHEMAS POST /rating/:id', () => { 
+    it(`Should answer 422, if user send food rating that isn't a number or is null`, async () => {
+        const ratingData: ratingInfo = await __createRating();
+        const token: string = await __createToken();
+
+        const placeData: placeInfo = await __createRestaurant();
+
+        await server.post('/places/create').set("Authorization",token).send(placeData);
+        const { body }: { body: foodPlaces[] } = await server.get('/places').send({});
+        const { status, text }: { status: number, text: string } = await server.post(`/rating/${body[0].id}`)
+
+    });
+});
+
+afterAll(async() => { 
+    await disconnectPrisma();
+});
