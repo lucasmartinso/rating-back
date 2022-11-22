199f0406b578d24ec9d54d7785f7c0e45ba648c4
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-11-22 10:00:07 -0300

    test: price, attendance and environment schema tests is done

--- tests/schemas/rating/create-schema.test.ts
+++ tests/schemas/rating/create-schema.test.ts
@@ -30,6 +30,48 @@ describe('TEST SCHEMAS POST /rating/:id', () => {
         expect(status).toBe(httpStatus.UNPROCESSABLE_ENTITY);
         expect(text).toContain(errorMessage);
     });
+
+    it(`Should answer 422, if user send environment rating that isn't a number or is null`, async () => {
+        const ratingData: ratingInfo = await __createRating();
+        const token: string = await __createToken();
+        const errorMessage: string = 'Environment field has to be a number'
+
+        const placeData: placeInfo = await __createRestaurant();
+        await server.post('/places/create').set("Authorization",token).send(placeData);
+        const { body }: { body: foodPlaces[] } = await server.get('/places').send({});
+        const { status, text }: { status: number, text: string } = await server.post(`/rating/${body[0].id}`).set("Authorization",token).send({ ...ratingData, environment: 'environment'})
+        
+        expect(status).toBe(httpStatus.UNPROCESSABLE_ENTITY);
+        expect(text).toContain(errorMessage);
+    });
+
+    it(`Should answer 422, if user send attendance rating that isn't a number or is null`, async () => {
+        const ratingData: ratingInfo = await __createRating();
+        const token: string = await __createToken();
+        const errorMessage: string = 'Attendance field has to be a number'
+
+        const placeData: placeInfo = await __createRestaurant();
+        await server.post('/places/create').set("Authorization",token).send(placeData);
+        const { body }: { body: foodPlaces[] } = await server.get('/places').send({});
+        const { status, text }: { status: number, text: string } = await server.post(`/rating/${body[0].id}`).set("Authorization",token).send({ ...ratingData, attendance: 'attendance'})
+        
+        expect(status).toBe(httpStatus.UNPROCESSABLE_ENTITY);
+        expect(text).toContain(errorMessage);
+    });
+
+    it(`Should answer 422, if user send price rating that isn't a number or is null`, async () => {
+        const ratingData: ratingInfo = await __createRating();
+        const token: string = await __createToken();
+        const errorMessage: string = 'Price field has to be a number'
+
+        const placeData: placeInfo = await __createRestaurant();
+        await server.post('/places/create').set("Authorization",token).send(placeData);
+        const { body }: { body: foodPlaces[] } = await server.get('/places').send({});
+        const { status, text }: { status: number, text: string } = await server.post(`/rating/${body[0].id}`).set("Authorization",token).send({ ...ratingData, price: 'price'})
+        
+        expect(status).toBe(httpStatus.UNPROCESSABLE_ENTITY);
+        expect(text).toContain(errorMessage);
+    });
 });
 
 afterAll(async() => { 
