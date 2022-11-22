66cfa4435603754b437bd3816b56c80d348af7c8
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-11-22 10:19:11 -0300

    feat: add personalizeted message to place schema

--- src/schemas/placesSchema.ts
+++ src/schemas/placesSchema.ts
@@ -2,18 +2,18 @@ import joi from "joi"
 
 export const restaurantSchema = joi.object({
     name: joi.string().min(2).max(30).required().label('Name must be between 2 and 30 characters'), 
-    description: joi.string(),
-    website: joi.string().uri(),
-    mainPhoto: joi.string().uri().required(),
-    type: joi.string().required(),
-    city: joi.string().min(2).max(50).required(),
-    address: joi.string().required()
+    description: joi.string().label('Description needs to be string and brief'),
+    website: joi.string().uri().label('Website needs to be in url format'),
+    mainPhoto: joi.string().uri().required().label('Photo needs to be in url format'),
+    type: joi.string().required().label('Unavailable type'),
+    city: joi.string().required().label('Unavailable city'),
+    address: joi.string().required().label('Unavailable address')
 })
 
 export const websiteSchema = joi.object({ 
-    website: joi.string().uri().required()
+    website: joi.string().uri().required().label('Website needs to be in url format')
 })
 
 export const descriptionSchema = joi.object({ 
-    description: joi.string().required()
+    description: joi.string().required().label('Description needs to be string and brief')
 })
